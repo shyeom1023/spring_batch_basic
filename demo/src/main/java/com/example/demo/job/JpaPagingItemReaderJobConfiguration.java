61e6316dfd9623df383cac0ef89be6b59f704a06
shyeom1023 <shyeom1023@gmail.com>
2021-06-10 19:33:52 +0900

    여러가지 job 추가함

---
+++ demo/src/main/java/com/example/demo/job/JpaPagingItemReaderJobConfiguration.java
@@ -0,0 +1,69 @@
+package com.example.demo.job;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.domain.Pay;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j // log 사용을 위한 lombok 어노테이션
+@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
+@Configuration
+public class JpaPagingItemReaderJobConfiguration {
+	
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    private int chunkSize = 2;
+
+    @Bean
+    public Job jpaPagingItemReaderJob() {
+        return jobBuilderFactory.get("jpaPagingItemReaderJob")
+                .start(jpaPagingItemReaderStep())
+                .build();
+    }
+    
+    @Bean
+    public Step jpaPagingItemReaderStep() {
+        return stepBuilderFactory.get("jpaPagingItemReaderStep")
+                .<Pay, Pay>chunk(chunkSize)
+                .reader(jpaPagingItemReader())
+                .writer(jpaPagingItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaPagingItemReader<Pay> jpaPagingItemReader() {
+		// TODO Auto-generated method stub
+		return new JpaPagingItemReaderBuilder<Pay>()
+				.name("jpaPagingItemReader")
+				.entityManagerFactory(entityManagerFactory)
+				.pageSize(chunkSize)
+				.queryString("SELECT p FROM Pay p WHERE amount >= 2000 ")
+				.build();
+	}
+	
+	private ItemWriter<Pay> jpaPagingItemWriter() {
+		// TODO Auto-generated method stub
+		return list -> {
+			for(Pay pay : list) {
+				log.info("Current Pay={}",pay);
+			}
+		};
+	}
+	
+}
+
